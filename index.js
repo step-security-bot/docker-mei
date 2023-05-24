c6feda6ccbda0edfdd2dffadaed93880ca1bfbf1
musicEnfanthen <stefan.muennich@unibas.ch>
2023-05-24 22:26:28 +0200

    fix(verovio): use module initialization for verovio and log version of toolkit

--- index.js
+++ index.js
@@ -18,45 +18,51 @@ const verovioOptions = {
    "svgViewBox": true
 }
 
-let stream = walker(['music-encoding/build/assets/images/GeneratedImages'])
+/* Wait for verovio to load */
+verovio.module.onRuntimeInitialized = () => {
+  
+  const tk = new verovio.toolkit()
+  console.log('Using Verovio toolkit version: ' + tk.getVersion())
 
-let processFile = (path) => {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
+  let stream = walker(['music-encoding/build/assets/images/GeneratedImages'])
 
-    const tk = new verovio.toolkit()
+  let processFile = (path) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
 
-    try {
-      let svg = tk.renderData(data, verovioOptions);
-      fs.writeFile(path + '.svg', svg, writingError => {
-        if (err) {
-          console.error(writingError)
-          return
-        }
-        //file written successfully
-      })
-      console.log('\x1b[32m%s\x1b[0m', 'Successfully rendered ' + path)
-    } catch(renderError) {
-      console.log('Unable to render ' + path + ':')
-      //console.error(renderError)
-    }
-    //console.log(data)
-  });
+      try {
+        let svg = tk.renderData(data, verovioOptions);
+        fs.writeFile(path + '.svg', svg, writingError => {
+          if (err) {
+            console.error(writingError)
+            return
+          }
+          //file written successfully
+        })
+        console.log('\x1b[32m%s\x1b[0m', 'Successfully rendered ' + path)
+       
+      } catch(renderError) {
+        console.log('Unable to render ' + path + ':')
+        //console.error(renderError)
+      }
+      //console.log(data)
 
-}
-
-
-
-stream.on('data', function (data) {
-
-  if (data.basename.endsWith('.mei') || data.basename.endsWith('.xml')) {
-    let path = data.filepath
-
-    processFile(path)
-
+    });
   }
 
-})
+
+  stream.on('data', function (data) {
+
+    if (data.basename.endsWith('.mei') || data.basename.endsWith('.xml')) {
+      let path = data.filepath
+      
+      processFile(path)
+
+    }
+
+  })
+
+}
