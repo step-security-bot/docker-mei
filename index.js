b1961e392ca250a8d220bbb1351bb2ac5f65ee2c
musicEnfanthen <stefan.muennich@unibas.ch>
2023-05-24 22:25:11 +0200

    fix(verovio): remove obsolote console logs from index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import walker from 'folder-walker'
 import fs from 'fs'
 import verovio from 'verovio'
 
-//process.argv.forEach((val, index, array) => {
-//  console.log(index + ': ' + val);
-//});
-
 const verovioOptions = {
    "scale": 10,
    "pageWidth": 1500,
@@ -33,7 +29,6 @@ let processFile = (path) => {
 
     const tk = new verovio.toolkit()
 
-    //console.log('received ' + typeof data + ' at ' + path)
     try {
       let svg = tk.renderData(data, verovioOptions);
       fs.writeFile(path + '.svg', svg, writingError => {
@@ -44,8 +39,6 @@ let processFile = (path) => {
         //file written successfully
       })
       console.log('\x1b[32m%s\x1b[0m', 'Successfully rendered ' + path)
-      // console.log('%cSuccessfully rendered ' + path, 'color: green;')
-      //console.log(typeof svg)
     } catch(renderError) {
       console.log('Unable to render ' + path + ':')
       //console.error(renderError)
